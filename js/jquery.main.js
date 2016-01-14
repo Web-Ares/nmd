667434bc4441c8c136069debe4d27df5079724c5
Sergey <kuzmenkos1986@gmail.com>
2016-01-14 15:51:24 +0200

    slider

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,36 +1,63 @@
 $(function(){
 
+    $('.swiper-container').each(function () {
+        Slider($(this));
+    });
+
+    $('.gallery__item a').on({
+        'click':function(){
+            $('.lb-dataContainer').prependTo( $('.lightbox') );
+            console.log(1)
+
+        }
+    });
 } );
-
-var Shablon = function (obj) {
-    this.obj = obj;
-
-
-    this.init();
-};
-Shablon.prototype = {
-    init: function () {
-        var self = this;
-
-        self.core = self.core();
-        self.core.build();
-    },
-    core: function () {
-        var self = this;
-
-        return {
-            addEvents: function () {
-
-            },
-            build: function () {
-                self.core.addEvents();
-            }
-        };
-    }
-};
 
 $(window).on({
     load: function () {
         
     }
 });
+
+var Slider = function (obj) {
+
+    //private properties
+    var _self = this,
+        _obj = obj;
+
+    //private methods
+    var _addEvents = function () {
+
+        },
+        _init = function () {
+            _addEvents();
+        };
+    if (_obj.hasClass('gallery__wrap')) {
+        var _slider = new Swiper(_obj, {
+            nextButton: '.gallery__next',
+            prevButton: '.gallery__prev',
+            spaceBetween:20,
+            loop: true,
+            slidesPerView: 4,
+            breakpoints: {
+                1024: {
+                    slidesPerView: 3
+                },
+                767: {
+                    slidesPerView: 2
+                },
+                500: {
+                    slidesPerView: 1
+                }
+            }
+
+        });
+
+    }
+
+    //public properties
+
+    //public methods
+
+    _init();
+};
